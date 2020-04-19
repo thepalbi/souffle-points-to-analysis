58cd03091d290f39a54340a43d5eb346a2c8c84c
Pablo Balbi <pablo.l.balbi@gmail.com>
2020-04-19 19:09:06 -0300

    WIP

---
+++ PointsToAnalysis/ModuleUnderTest/src/main/java/wtf/thepalbi/ClassUnderTest1.java
@@ -0,0 +1,18 @@
+package wtf.thepalbi;
+
+public class ClassUnderTest1 {
+    private String someString;
+
+    public static void main(String[] args) {
+        ClassUnderTest1 test = new ClassUnderTest1();
+        String b = new String();
+        b += " holis";
+        test.someString = b;
+        String c = test.method2(b);
+        System.out.println(c);
+    }
+
+    public String method2(String gola) {
+        return gola + " gola";
+    }
+}
--- PointsToAnalysis/src/test/java/wtf/thepalbi/TranslatorTests.java
+++ PointsToAnalysis/src/test/java/wtf/thepalbi/TranslatorTests.java
@@ -2,57 +2,52 @@ package wtf.thepalbi;
 
 import org.junit.Test;
 import soot.Body;
+import soot.JastAddJ.Opt;
 import soot.PackManager;
 import soot.Scene;
 import soot.options.Options;
-import wtf.thepalbi.utils.UUIDHeapLocationFactory;
 
-import javax.swing.text.html.Option;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
-import static java.util.Arrays.parallelSort;
 
 /**
  * Unit test for simple App.
  */
 public class TranslatorTests {
 
-    public Body getBodyForClassAndMethod(String innerClassName, String methodName) {
-        String fullyQualifiedName = this.getClass().getCanonicalName() + "$" + innerClassName;
-
+    public Body getBodyForClassAndMethod(String targetClassName, String methodName) {
         Options.v().setPhaseOption("jb", "use-original-names: true");
 
-        // This should be the path to the test-classes directory
-        String pathToRunningTestJar = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        String pathToTargetClasses = pathToRunningTestJar.replace("test-classes", "test-classes");
+        // Setting soot class path. ITS NOT THE SAME AS PROCESS DIR
+        // Adding soot classpath
 
         // Set as classpath both test and src classes
-        Options.v().set_process_dir(asList(pathToRunningTestJar, pathToTargetClasses));
-        // Avoid errors from resolving transitive dependencies
-        Options.v().set_allow_phantom_refs(true);
+        Options.v().set_process_dir(asList("/Users/thepalbi/Facultad/aap/souffle-points-to-analysis/PointsToAnalysis/ModuleUnderTest/target/classes"));
         // Add rt.jar in classpath
         Options.v().set_prepend_classpath(true);
         // Output format is Jimple, so JBP wil be used
         Options.v().set_output_format(Options.output_format_jimple);
         // Both of these are needed for loading target classes
-        Scene.v().loadNecessaryClasses();
         // Keep line numbers to craft invocation sites
         Options.v().set_keep_line_number(true);
+
+        Options.v().set_main_class(targetClassName);
+
+        Scene.v().loadNecessaryClasses();
 
         PackManager.v().runBodyPacks();
 
         // Get class first, then get desired method
-        return Scene.v().getSootClass(fullyQualifiedName).getMethodByName(methodName).getActiveBody();
+        return Scene.v().getSootClass(targetClassName).getMethodByName(methodName).getActiveBody();
     }
 
     // TODO: Start adding test cases just counting the expected fact count of some type to the Jimple translation
 
     @Test
     public void simpleTranslation() throws Exception {
-        Body methodBody = getBodyForClassAndMethod("SomeStringGetsCreated", "main");
+        Body methodBody = getBodyForClassAndMethod("wtf.thepalbi.ClassUnderTest1", "main");
         PointToAnalysis analysis = new PointToAnalysis();
 
         String targetPackage = "wtf.thepalbi";
@@ -64,22 +59,5 @@ public class TranslatorTests {
                 .collect(Collectors.toList());
 
         analysis.main(targetBodies, methodBody, Scene.v());
-    }
-
-    // Class under test located in the same @Test class, to make things more clear
-    public static class SomeStringGetsCreated {
-        public String someString;
-
-        public static void main(String[] args) {
-            SomeStringGetsCreated someStringGetsCreated = new SomeStringGetsCreated();
-            String b = new String();
-            someStringGetsCreated.someString = new String();
-            String c = someStringGetsCreated.method2(b);
-            System.out.println(c);
-        }
-
-        public String method2(String gola) {
-            return gola + " gola";
-        }
     }
 }
